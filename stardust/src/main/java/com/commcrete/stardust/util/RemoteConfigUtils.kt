2ec28493641cc83e8afe17e0f2dc56a543559a27
Shlomo <Shlomo.A@commcrete.co.il>
2024-08-29 15:33:56 +0300

    added remote config fix

--- stardust/src/main/java/com/commcrete/stardust/util/RemoteConfigUtils.kt
+++ stardust/src/main/java/com/commcrete/stardust/util/RemoteConfigUtils.kt
@@ -3,10 +3,9 @@ package com.commcrete.stardust.util
 import android.content.Context
 import com.commcrete.stardust.R
 
-class RemoteConfigUtils {
+object RemoteConfigUtils {
 
-
-    private fun initLocalDefaults (context: Context) {
+    fun initLocalDefaults (context: Context) {
         val offlineConfig = FileUtils.readRawResourceAsByteArray(context, R.raw.offline_demo    )
         val newDemoConfig = FileUtils.readRawResourceAsByteArray(context, R.raw.new_demo    )
         val fileOfflineDemo = FileUtils.createFile(context, folderName = "config", fileName = "offlineDemo", fileType = ".json")
